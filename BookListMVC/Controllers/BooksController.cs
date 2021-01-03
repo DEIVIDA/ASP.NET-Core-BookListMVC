c294e57e7001fcece46a2da3de5571d050f3f12c
Deivida Gaizauskaite <info@dwebseo.eu>
2021-01-03 23:49:09 +0200

    BookList Js and API Calls

--- BookListMVC/Controllers/BooksController.cs
+++ BookListMVC/Controllers/BooksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,37 @@ namespace BookListMVC.Controllers
 {
     public class BooksController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookfromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookfromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Books.Remove(bookfromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+        #endregion
     }
 }
