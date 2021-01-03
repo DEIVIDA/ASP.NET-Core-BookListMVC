76f8d71171366fe14a1bcea9756deed809f81ec3
Deivida Gaizauskaite <info@dwebseo.eu>
2021-01-04 00:45:37 +0200

    Upsert Post And Delete

--- BookListMVC/Controllers/BooksController.cs
+++ BookListMVC/Controllers/BooksController.cs
@@ -42,6 +42,27 @@ namespace BookListMVC.Controllers
             return View(Book);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Book.Id == 0)
+                {
+                    //create
+                    _db.Books.Add(Book);
+                }
+                else
+                {
+                    _db.Books.Update(Book);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Book);
+        }
+
         #region API Calls
         [HttpGet]
         public async Task<IActionResult> GetAll()
